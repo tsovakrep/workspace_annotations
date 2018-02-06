3c5d5db536b4a12d248dcfbab6cdfbbb0f4b9bcc
TsovakPalakian <tsovakpalakian@gmail.com>
2018-02-06 22:43:44 +0300

    autocommit all files

--- classloader/src/by/htp/itacademy/Module.java
+++ classloader/src/by/htp/itacademy/Module.java
@@ -2,7 +2,7 @@ package by.htp.itacademy;
 
 public interface Module {
 	
-	public static final int EXIT_SECCESS = 0;
+	public static final int EXIT_SUCCESS = 0;
 	public static final int EXIT_FAILURE = 1;
 	
 	void load();
---
+++ classloader/src/by/htp/itacademy/ModulePrinter.java
@@ -0,0 +1,20 @@
+package by.htp.itacademy;
+
+public class ModulePrinter implements Module {
+
+	@Override
+	public void load() {
+		System.out.println("Module " + this.getClass() + " loading ...");
+	}
+
+	@Override
+	public int run() {
+		System.out.println("Module " + this.getClass() + " running ...");
+		return Module.EXIT_SUCCESS;
+	}
+
+	@Override
+	public void unload() {
+		System.out.println("Module " + this.getClass() + " inloading ...");
+	}
+}
