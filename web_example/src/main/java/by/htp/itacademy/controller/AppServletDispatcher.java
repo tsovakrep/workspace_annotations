bb5a1a7a9abbf73cb88d01274eb2e5ea282e2fb6
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-02 18:08:10 +0300

    autocommit all files

--- web_example/src/main/java/by/htp/itacademy/controller/AppServletDispatcher.java
+++ web_example/src/main/java/by/htp/itacademy/controller/AppServletDispatcher.java
@@ -2,11 +2,16 @@ package by.htp.itacademy.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import annotationapi.util.AnnotationFinder;
+import annotationapi.util.MethodContainer;
 
 @SuppressWarnings("serial")
 public class AppServletDispatcher extends HttpServlet {
@@ -20,6 +25,33 @@ public class AppServletDispatcher extends HttpServlet {
 		System.out.println("request.getRequestURI(): " + request.getRequestURI());
 //		RequestDispatcher rd = request.getRequestDispatcher("");
 //		rd.forward(request, response);
+		
+		ServletContext sc = request.getServletContext();
+		AnnotationFinder af = (AnnotationFinder) sc.getAttribute("annotationfinder");
+
+		String uri = request.getRequestURI();
+		String[] arrUri = uri.split("/");
+		for (String string : arrUri) {
+			System.out.println(string);
+		}
+
+		int index = arrUri.length - 2;
+
+		Map<String, MethodContainer> methodContainer = af.getMethodContainer();
+		for (Map.Entry<String, MethodContainer> meth : methodContainer.entrySet()) {
+			System.out.println();
+			System.out.println(meth.getKey() + " : " + meth.getValue());
+			if (meth.getKey().endsWith("}")) {
+				String[] arrMeth = meth.getKey().split("/");
+
+				if (arrUri[index].equals(arrMeth[index])) {
+					uri = meth.getKey();
+				}
+			}
+		}
+		System.out.println(arrUri[arrUri.length - 1]);
+		System.out.println("uri: " + uri);
+		System.out.println(methodContainer);
 	}
 
 	@Override
