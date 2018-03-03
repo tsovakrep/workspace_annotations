11c5bc892bf1409936d410f00811ab661b02dacf
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-03 04:50:47 +0300

    autocommit all files

--- web_example/src/main/java/annotationapi/util/ResponseEntity.java
+++ web_example/src/main/java/annotationapi/util/ResponseEntity.java
@@ -1,5 +1,35 @@
 package annotationapi.util;
 
-public class ResponseEntity {
+public class ResponseEntity<T> {
+	
+	private T body;
+	private String page;
+	private HttpStatus status;
+
+	public ResponseEntity(HttpStatus status) {
+		this.status = status;
+	}
+
+	public ResponseEntity(T body, HttpStatus status) {
+		this(status);
+		this.body = body;
+	}
+
+	public ResponseEntity(T body, String page, HttpStatus status) {
+		this(body, status);
+		this.page = page;
+	}
+
+	public T getBody() {
+		return body;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
 	
 }
