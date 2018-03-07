408a37f9232ce779ea04cacb830910d3e4373e77
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-07 22:17:38 +0300

    autocommit all files

--- web_example/src/main/java/annotationapi/util/MethodDispatcher.java
+++ web_example/src/main/java/annotationapi/util/MethodDispatcher.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -32,27 +33,33 @@ public class MethodDispatcher {
 			ServletException, IOException {
 
 		ServletContext sc = request.getServletContext();
+		Set<String> pages = (Set<String>) sc.getAttribute("pages");
+		ServletRegistration jsp = sc.getServletRegistration("jsp");
+		for (String string : pages) {
+			jsp.addMapping(string);
+		}
 
 //		ServletRegistration servlet1 = sc.getServletRegistration("dispatcher");
 //		servlet1.addMapping("/welcome/Tsovak");
 
-//		for (Map.Entry<String, ? extends ServletRegistration> servlet : sc.getServletRegistrations().entrySet()) {
-//			ServletRegistration servletReg = servlet.getValue();
-//			// if (servletReg.getClassName().startsWith("by.htp")) {
-//			System.out.println();
-//			if (AppServletDispatcher.class.getName().equals(servlet.getValue().getClassName())) {
-//				servletReg.addMapping("/go/ts");
-//				servletReg.addMapping("/welcome");
-//				servletReg.addMapping("/welcome/tsovak");
-//				servletReg.addMapping("/welcome/palakian");
-//				servletReg.addMapping("/callMethod/ts");
-//			}
-//			// }
-//			System.out.println(servlet.getKey() + " : " + servletReg.getClassName());
-//			for (String url : servletReg.getMappings()) {
-//				System.out.println("url mapping" + " : " + url);
-//			}
-//		}
+		for (Map.Entry<String, ? extends ServletRegistration> servlet : sc.getServletRegistrations().entrySet()) {
+			ServletRegistration servletReg = servlet.getValue();
+			// if (servletReg.getClassName().startsWith("by.htp")) {
+			System.out.println();
+			if (AppServletDispatcher.class.getName().equals(servlet.getValue().getClassName())) {
+				servletReg.addMapping("/go/ts");
+				servletReg.addMapping("/welcome");
+				servletReg.addMapping("/welcome/tsovak");
+				servletReg.addMapping("/welcome/palakian");
+				servletReg.addMapping("/callMethod/ts");
+			}
+			// }
+			System.out.println(servlet.getKey() + " : " + servletReg.getClassName());
+			for (String url : servletReg.getMappings()) {
+				System.out.println("url mapping" + " : " + url);
+			}
+		}
+		
 		AnnotationFinder af = (AnnotationFinder) sc.getAttribute("annotationfinder");
 
 		Map<String, ServletContainer> methodContainerMap = af.getMethodContainer();
