5fa1f2699f246d2072539a3b5f3fd9677a80cfc3
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-17 02:38:30 +0300

    autocommit all files

--- web_example/src/main/java/by/htp/itacademy/controller/UserValidation.java
+++ web_example/src/main/java/by/htp/itacademy/controller/UserValidation.java
@@ -8,27 +8,22 @@ import framework.webcore.bean.Validation;
 
 @Component
 public class UserValidation extends Validation {
-	
-	private static final Map<String, String> USER_REGEX_PARAMETERS = new ConcurrentHashMap<String, String>();
-
-	static {
-		USER_REGEX_PARAMETERS.put("login", "[A-z0-9]{3,20}");
-		USER_REGEX_PARAMETERS.put("password", "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\!\\@\\#\\$\\%\\^\\&\\*]).{8,100})");
-		USER_REGEX_PARAMETERS.put("name", "[A-Z]{1}[a-z]{0,100}");
-		USER_REGEX_PARAMETERS.put("surname", "[A-Z]{1}[a-z]{0,100}");
-		USER_REGEX_PARAMETERS.put("email", "^[a-zA-Z0-9.,_%+-]+@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,4}$");
-		USER_REGEX_PARAMETERS.put("passport", "[A-Z]{2}[0-9]{7}");
-		USER_REGEX_PARAMETERS.put("phoneNumber", "[\\+]{1}[0-9]{2,4}[0-9]+");
-	}
-	
-	static {
-		
-	}
 
 	@Override
 	protected Map<Class<?>, Map<String, String>> setRegexParameters(Class<?> clazz,
 			Map<String, String> mapOfClassFieldAndRegex) {
-		REGEX_PARAMETERS.put(User.class, USER_REGEX_PARAMETERS);
+		
+		Map<String, String> user_regex_parameters = new ConcurrentHashMap<String, String>();
+		
+		user_regex_parameters.put("login", "[A-z0-9]{3,20}");
+		user_regex_parameters.put("password", "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\!\\@\\#\\$\\%\\^\\&\\*]).{8,100})");
+		user_regex_parameters.put("name", "[A-Z]{1}[a-z]{0,100}");
+		user_regex_parameters.put("surname", "[A-Z]{1}[a-z]{0,100}");
+		user_regex_parameters.put("email", "^[a-zA-Z0-9.,_%+-]+@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,4}$");
+		user_regex_parameters.put("passport", "[A-Z]{2}[0-9]{7}");
+		user_regex_parameters.put("phoneNumber", "[\\+]{1}[0-9]{2,4}[0-9]+");
+		
+		REGEX_PARAMETERS.put(User.class, user_regex_parameters);
 		return REGEX_PARAMETERS;
 	}
 }
