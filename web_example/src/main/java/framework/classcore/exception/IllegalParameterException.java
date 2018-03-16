2a0a35c165faf0a98b32eb4ce9dabc6ab3215b29
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-16 23:38:30 +0300

    autocommit all files

---
+++ web_example/src/main/java/framework/classcore/exception/IllegalParameterException.java
@@ -0,0 +1,22 @@
+package framework.classcore.exception;
+
+@SuppressWarnings("serial")
+public class IllegalParameterException extends Exception {
+
+	public IllegalParameterException() {
+		super();
+	}
+
+	public IllegalParameterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public IllegalParameterException(String message) {
+		super(message);
+	}
+
+	public IllegalParameterException(Throwable cause) {
+		super(cause);
+	}
+	
+}
---
+++ web_example/src/main/java/framework/classcore/util/Methods.java
@@ -0,0 +1,77 @@
+package framework.classcore.util;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import framework.classcore.exception.IllegalParameterException;
+
+public final class Methods {
+
+	private Methods() {
+	}
+
+	protected static Class<?>[] getParameterTypes(Object obj, byte count) 
+			throws IllegalParameterException {
+		
+		if (obj == null) {
+			throw new IllegalParameterException("An object can not to be null!");
+		}
+
+		Constructor<?>[] constructors = obj.getClass().getConstructors();
+
+		Class<?>[] parameterTypes = null;
+		
+		for (int i = 0; i < constructors.length; ++i) {
+			if (constructors[i].getParameterCount() == count) {
+				parameterTypes = constructors[i].getParameterTypes();
+			}
+		}
+		
+		if (parameterTypes == null) {
+			throw new IllegalParameterException("A constructor with such a number of parameters does not exist!");
+		}
+		
+		return null;
+	}
+	
+	protected static Field[] getDeclaredFields(Object obj) 
+			throws IllegalParameterException {
+		
+		if (obj == null) {
+			throw new IllegalParameterException("An object can not to be null!");
+		}
+		
+		return obj.getClass().getDeclaredFields();
+	}
+	
+	protected static Method[] getDeclaredMethods(Object obj) 
+			throws IllegalParameterException {
+		
+		if (obj == null) {
+			throw new IllegalParameterException("An object can not to be null!");
+		}
+		
+		return obj.getClass().getDeclaredMethods();
+	}
+	
+	protected static Class<?> getFieldClass(Field field) 
+			throws ClassNotFoundException, IllegalParameterException {
+		
+		if (field == null) {
+			throw new IllegalParameterException("A field can not to be null!");
+		}
+		
+		return Class.forName(field.getType().getName());
+	}
+
+	protected static Constructor<?>[] getConstructors(Object obj) 
+			throws IllegalParameterException, SecurityException, ClassNotFoundException {
+		
+		if (obj == null) {
+			throw new IllegalParameterException("An object can not to be null!");
+		}
+		
+		return Class.forName(obj.getClass().getName()).getConstructors();
+	}
+}
