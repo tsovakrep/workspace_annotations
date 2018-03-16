f2596960c2263976f41f407c0144814778a68650
TsovakPalakian <tsovakpalakian@gmail.com>
2018-03-16 21:58:30 +0300

    autocommit all files

--- web_example/src/main/java/framework/classcore/annotation/validation/Validation.java
+++ web_example/src/main/java/framework/classcore/annotation/validation/Validation.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-
 @Documented
 @Target({FIELD, PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
